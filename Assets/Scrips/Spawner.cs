9da87d14db4c4fce3a4b35dc5410d9bd521a2429
OvcS <gerkules007@yahoo.com>
2021-12-05 16:51:38 +0300

    Add C# Script for newblock and Frequency Appear for new block

--- Assets/Scrips/Spawner.cs
+++ Assets/Scrips/Spawner.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public float minX, maxX, YRangeMin, YRangeMax, cameraDistance;
+    public float minX, maxX, YRangeMin, YRangeMax, cameraDistance, frequencyAppear;
     public Transform platformPrefab;
-    private Transform cam;
-    private float lastSpawnY;
+    public Transform platformPrefabDisappear;
+    private Transform cam, platform;
+    private float lastSpawnY, lastSpawnDisappear, AppearRange, newBlockPoint;
     void Start()
     {
         cam = Camera.main.transform;
         lastSpawnY = -7;
+        AppearRange = 101 - frequencyAppear;
     }
 
     void Update()
@@ -19,10 +21,19 @@ public class Spawner : MonoBehaviour
         // Как только камера выше последней позиции персонажа
         if (cam.position.y + cameraDistance > lastSpawnY)
         {
+            newBlockPoint = Random.Range(0, 101);
+            if (newBlockPoint > AppearRange)
+            {
+                platform = Instantiate(platformPrefabDisappear,
+                    new Vector3(Random.Range(minX, maxX), lastSpawnY + Random.Range(YRangeMin, YRangeMax), 0), Quaternion.identity);
+            }
             // Для платформы - создаем объект из ничего (Insantiate) из платформы для прыжка в случайном положении на карте
             // где платформа по высоте изменяется каждый раз от места появления персонаж для игры
-            Transform platform = Instantiate(platformPrefab, 
-                new Vector3( Random.Range(minX, maxX), lastSpawnY + Random.Range(YRangeMin,YRangeMax), 0 ), Quaternion.identity);
+            else
+            {
+                platform = Instantiate(platformPrefab,
+                    new Vector3(Random.Range(minX, maxX), lastSpawnY + Random.Range(YRangeMin, YRangeMax), 0), Quaternion.identity);
+            }
             // Сохраняем позицию персонажа для дальнейшего расчета
             lastSpawnY = platform.position.y;
         }
---
+++ Assets/Scrips/SpawnerDisappear.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerDisappear : MonoBehaviour
+{
+    public float lastDissapearSpawn;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
